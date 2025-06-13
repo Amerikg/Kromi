0763364ff0228c436c4dee5f59e73f661624bbd1
Amerikg <amelizruiz@gmail.com>
2025-06-13 11:25:29 -0600

    delete 5 files

---
+++ kromi/kromi/src/main/java/com/kromi/KromiApplication.java
@@ -0,0 +1,13 @@
+package com.kromi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KromiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KromiApplication.class, args);
+	}
+
+}
---
+++ kromi/kromi/src/test/java/com/kromi/KromiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kromi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KromiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
